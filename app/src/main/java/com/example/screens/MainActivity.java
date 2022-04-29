d568de69bac6fb6ef15a1ab32de2448e3c98677d
b21ricni <b21ricni@student.his.se>
2022-04-29 11:22:45 +0200

    Logs

--- app/src/main/java/com/example/screens/MainActivity.java
+++ app/src/main/java/com/example/screens/MainActivity.java
@@ -3,8 +3,33 @@ package com.example.screens;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onPostResume() {
+        super.onPostResume();
+        Log.d("==>","onPostResume");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("==>","OnStart");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("==>","OnStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("==>","OnDestroy");
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
